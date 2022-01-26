f4a01c3af306409323bb1f768655f05244a12fcb
Murtakhan <murtahan.aglarov@gmail.com>
2022-01-26 17:43:27 +0300

    добавили авторизацию

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
