021b1bfdd4ec9a37fc86e3c8728b791739a1c606
Murtakhan <murtahan.aglarov@gmail.com>
2022-01-26 17:53:22 +0300

    Добавили миграцию

---
+++ database/migrations/2022_01_26_144553_create_links_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('origin_href', 255);
+            $table->string('short_link', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
